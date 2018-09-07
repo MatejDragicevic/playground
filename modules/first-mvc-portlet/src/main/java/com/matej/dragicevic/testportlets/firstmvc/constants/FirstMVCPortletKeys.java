8f225b524e525188d22aeb31f979cf9115a890a6
Matej Dragicevic <matej.dragicevic@mercatis.com>
2018-09-07 09:02:36 +0200

    init

---
+++ modules/first-mvc-portlet/src/main/java/com/matej/dragicevic/testportlets/firstmvc/constants/FirstMVCPortletKeys.java
@@ -0,0 +1,10 @@
+package com.matej.dragicevic.testportlets.firstmvc.constants;
+
+/**
+ * @author matej
+ */
+public class FirstMVCPortletKeys {
+
+	public static final String FirstMVC = "FirstMVC";
+
+}
---
+++ modules/first-mvc-portlet/src/main/resources/META-INF/resources/view.jsp
@@ -0,0 +1,5 @@
+<%@ include file="/init.jsp" %>
+
+<p>
+	<b><liferay-ui:message key="first-mvc-portlet.caption"/></b>
+</p>
